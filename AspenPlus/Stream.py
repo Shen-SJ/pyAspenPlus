ed1d057e22cca1cc0446fed5df2f1fa9a039d2b6
Shen_HJ <johnson840205@gmail.com>
2018-12-24 17:44:19 +0800

    V03 Merge branch 'Release'
    新增完整功能的Stream類別

--- AspenPlus/Stream.py
+++ AspenPlus/Stream.py
@@ -1,6 +1,8 @@
 # 匯入自定義錯誤，不然有些函式會找不到哪裡raise錯誤
 from AspenPlus import UserDifineException as UDE
 
+# TODO: 我總覺得這堆重複性很高的程式碼可以用decorator，
+#  不過我還不會，正在研究當中
 class Stream:
     def __init__(self, master):
         # 應該是不用辨認輸入的master是否為帶有路徑的AspenPlus類別
@@ -10,11 +12,11 @@ class Stream:
 
     def MoleFlow(self, sname, component='', get_unit=False, unit_change_to=[]) -> float:
         """Get the  Moleflow of specified stream for Total or component .
-        sname: Stream name
-        component: If it is no given. the fct will give the total moleflow.
-                   If you give the component name, the fct will give the component flowrate.
-        get_unit: "True" to get the unit. "False" is a default value.
-        unit_change_to: unit index in AspenFile for the changing unit.
+                sname: Stream name
+                component: If it is no given. the fct will give the total moleflow.
+                                If you give the component name, the fct will give the component flowrate.
+                get_unit: "True" to get the unit. "False" is a default value.
+                unit_change_to: unit index in AspenFile for the changing unit.
         """
         ## 先檢查輸入的資料型態正確與否
         if type(sname) != str:
@@ -62,6 +64,258 @@ class Stream:
             else:  # 取得數值
                 return self.master.UnitChange(self.aspen.Tree.FindNode(path), unit_change_to)
 
+    def MoleFrac(self, sname, component) -> float:
+        """Get the  Mole Fraction of specified component of specified stream.
+                sname: Stream name
+                component: component you want to know.
+        """
+        ## 先檢查輸入的資料型態正確與否
+        if type(sname) != str:
+            raise TypeError("sname must be a 'String'!!!")
+        if type(component) != str:
+            raise TypeError("component must be a 'String'!!!")
+
+        ## 把sname和component轉換成大寫，因為Aspen裡面的名稱都大寫
+        sname = sname.upper()
+        component = component.upper()
+
+        ## 檢查輸入的物流與化合物名稱是否在檔案當中，如無回傳錯誤
+        if sname not in self.master.StreamsList():
+            raise UDE.AspenPlus_StreamTypeError("Cannot Find " + sname
+                                                + " in the AspenFile. "
+                                                + "Please Check the name you type!!")
+        if component not in self.master.ComponentsList():
+            raise UDE.AspenPlus_ComponentTypeError("Cannot Find " + component
+                                                   + ' in the AspenFile. '
+                                                   + "Please Check the name you type!!")
+
+        # mole fraction這邊用算的
+        mt = self.MoleFlow(sname)
+        m = self.MoleFlow(sname, component=component)
+        return m/mt
+
+    def MassFlow(self, sname, component='', get_unit=False, unit_change_to=[]) -> float:
+        """Get the  Massflow of specified stream for Total or component .
+                sname: Stream name
+                component: If it is no given. the fct will give the total massflow.
+                                If you give the component name, the fct will give the component flowrate.
+                get_unit: "True" to get the unit. "False" is a default value.
+                unit_change_to: unit index in AspenFile for the changing unit.
+        """
+        ## 先檢查輸入的資料型態正確與否
+        if type(sname) != str:
+            raise TypeError("sname must be a 'String'!!!")
+        if type(component) != str:
+            raise TypeError("component must be a 'String'!!!")
+        if type(get_unit) != bool:
+            raise TypeError("unit must be a 'Boolean'!!!")
+        if type(unit_change_to) != list and type(unit_change_to) != int:
+            raise TypeError("unit_change must be a 'Integer'!!!")
+
+        ## 把sname和component轉換成大寫，因為Aspen裡面的名稱都大寫
+        sname = sname.upper()
+        component = component.upper()
+
+        ## 檢查輸入的物流名稱是否在檔案當中，如無回傳錯誤 (化合物辨認在後面
+        if sname not in self.master.StreamsList():
+            raise UDE.AspenPlus_StreamTypeError("Cannot Find " + sname
+                                                + " in the AspenFile. "
+                                                + "Please Check the name you type!!")
+
+        ## 辨認是要輸出總流量還是化合物流量
+        if component != '':
+            ## 辨認化合物有沒有在檔案當中，如無回傳錯誤
+            if component not in self.master.ComponentsList():
+                raise UDE.AspenPlus_ComponentTypeError("Cannot Find " + component
+                                                       + " in the AspenFile. "
+                                                       + "Please Check the name you type!!")
+
+            path = '\Data\Streams' + '\\' + sname + '\Output\MASSFLOW3' + '\\' + component
+        elif component == '':
+            path = "\Data\Streams" + "\\" + sname + "\Output\STR_MAIN\MASSFLMX\MIXED"
+
+        ## 看看有沒有要改單位
+        if unit_change_to == []:  # 沒有要改單位
+            ## 是要取得數值還是單位
+            if get_unit:  # 取得單位
+                return self.master.UnitFind(self.aspen.Tree.FindNode(path), table=True)
+            else:  # 取得數值
+                return self.aspen.Tree.FindNode(path).Value
+        elif unit_change_to != []:  # 要改單位的話
+            if get_unit:  # 取得單位
+                pq = self.aspen.Tree.FindNode(path).AttributeValue(2)
+                return self.master.Unit(item=[pq, unit_change_to], table=True)
+            else:  # 取得數值
+                return self.master.UnitChange(self.aspen.Tree.FindNode(path), unit_change_to)
+
+    def MassFrac(self, sname, component) -> float:
+        """Get the  Mass Fraction of specified component of specified stream.
+                sname: Stream name
+                component: component you want to know.
+        """
+        ## 先檢查輸入的資料型態正確與否
+        if type(sname) != str:
+            raise TypeError("sname must be a 'String'!!!")
+        if type(component) != str:
+            raise TypeError("component must be a 'String'!!!")
+
+        ## 把sname和component轉換成大寫，因為Aspen裡面的名稱都大寫
+        sname = sname.upper()
+        component = component.upper()
+
+        ## 檢查輸入的物流與化合物名稱是否在檔案當中，如無回傳錯誤
+        if sname not in self.master.StreamsList():
+            raise UDE.AspenPlus_StreamTypeError("Cannot Find " + sname
+                                                + " in the AspenFile. "
+                                                + "Please Check the name you type!!")
+        if component not in self.master.ComponentsList():
+            raise UDE.AspenPlus_ComponentTypeError("Cannot Find " + component
+                                                   + ' in the AspenFile. '
+                                                   + "Please Check the name you type!!")
+
+        # mass fraction這邊用算的
+        mt = self.MassFlow(sname)
+        m = self.MassFlow(sname, component=component)
+        return m/mt
+
+    def Temperature(self, sname, get_unit=False, unit_change_to=[]) -> float:
+        """Get the Temperature of specified stream.
+                sname: Stream name in AspenPlus.
+                get_unit: "True" to get the unit. "False" is a default value.
+                unit_change_to: unit index in AspenFile for the changing unit.
+        """
+        ## 先檢查輸入的資料型態正確與否
+        if type(sname) != str:
+            raise TypeError("sname must be a 'String'!!!")
+        if type(get_unit) != bool:
+            raise TypeError("unit must be a 'Boolean'!!!")
+        if type(unit_change_to) != list and type(unit_change_to) != int:
+            raise TypeError("unit_change must be a 'Integer'!!!")
+
+        ## 把sname轉換成大寫，因為Aspen裡面的名稱都大寫
+        sname = sname.upper()
+
+        ## 檢查輸入的物流名稱是否在檔案當中，如無回傳錯誤
+        if sname not in self.master.StreamsList():
+            raise UDE.AspenPlus_StreamTypeError("Cannot Find " + sname
+                                                + " in the AspenFile. "
+                                                + "Please Check the name you type!!")
+
+        path = "\Data\Streams" + "\\" + sname + "\Output\STR_MAIN\TEMP\MIXED"
+
+        ## 看看有沒有要改單位
+        if unit_change_to == []:  # 沒有要改單位
+            ## 是要取得數值還是單位
+            if get_unit:  # 取得單位
+                return self.master.UnitFind(self.aspen.Tree.FindNode(path), table=True)
+            else:  # 取得數值
+                return self.aspen.Tree.FindNode(path).Value
+        elif unit_change_to != []:  # 要改單位的話
+            if get_unit:  # 取得單位
+                pq = self.aspen.Tree.FindNode(path).AttributeValue(2)
+                return self.master.Unit(item=[pq, unit_change_to], table=True)
+            else:  # 取得數值
+                return self.master.UnitChange(self.aspen.Tree.FindNode(path), unit_change_to)
+
+    def Pressure(self, sname, get_unit=False, unit_change_to=[]) -> float:
+        """Get the Pressure of specified stream.
+                sname: Stream name in AspenPlus.
+                get_unit: "True" to get the unit. "False" is a default value.
+                unit_change_to: unit index in AspenFile for the changing unit.
+        """
+        ## 先檢查輸入的資料型態正確與否
+        if type(sname) != str:
+            raise TypeError("sname must be a 'String'!!!")
+        if type(get_unit) != bool:
+            raise TypeError("unit must be a 'Boolean'!!!")
+        if type(unit_change_to) != list and type(unit_change_to) != int:
+            raise TypeError("unit_change must be a 'Integer'!!!")
+
+        ## 把sname轉換成大寫，因為Aspen裡面的名稱都大寫
+        sname = sname.upper()
+
+        ## 檢查輸入的物流名稱是否在檔案當中，如無回傳錯誤
+        if sname not in self.master.StreamsList():
+            raise UDE.AspenPlus_StreamTypeError("Cannot Find " + sname
+                                                + " in the AspenFile. "
+                                                + "Please Check the name you type!!")
+
+        path = "\Data\Streams" + "\\" + sname + "\Output\STR_MAIN\PRES\MIXED"
+
+        ## 看看有沒有要改單位
+        if unit_change_to == []:  # 沒有要改單位
+            ## 是要取得數值還是單位
+            if get_unit:  # 取得單位
+                return self.master.UnitFind(self.aspen.Tree.FindNode(path), table=True)
+            else:  # 取得數值
+                return self.aspen.Tree.FindNode(path).Value
+        elif unit_change_to != []:  # 要改單位的話
+            if get_unit:  # 取得單位
+                pq = self.aspen.Tree.FindNode(path).AttributeValue(2)
+                return self.master.Unit(item=[pq, unit_change_to], table=True)
+            else:  # 取得數值
+                return self.master.UnitChange(self.aspen.Tree.FindNode(path), unit_change_to)
+
+    def VaporFrac(self, sname) -> float:
+        """Get the Vapor fraction of specified stream.
+                sname: Stream name in AspenPlus.
+                get_unit: "True" to get the unit. "False" is a default value.
+                unit_change_to: unit index in AspenFile for the changing unit.
+        """
+        ## 先檢查輸入的資料型態正確與否
+        if type(sname) != str:
+            raise TypeError("sname must be a 'String'!!!")
+
+        ## 把sname轉換成大寫，因為Aspen裡面的名稱都大寫
+        sname = sname.upper()
+
+        ## 檢查輸入的物流名稱是否在檔案當中，如無回傳錯誤
+        if sname not in self.master.StreamsList():
+            raise UDE.AspenPlus_StreamTypeError("Cannot Find " + sname
+                                                + " in the AspenFile. "
+                                                + "Please Check the name you type!!")
+
+        path = "\Data\Streams" + "\\" + sname + "\Output\STR_MAIN\VFRAC\MIXED"
+        return self.aspen.Tree.FindNode(path).Value
+
+    def VolumeFlow(self, sname, get_unit=False, unit_change_to=[]) -> float:
+        """Get the volumetric flowrate of specified stream.
+                sname: Stream name in AspenPlus.
+                get_unit: "True" to get the unit. "False" is a default value.
+                unit_change_to: unit index in AspenFile for the changing unit.
+        """
+        ## 先檢查輸入的資料型態正確與否
+        if type(sname) != str:
+            raise TypeError("sname must be a 'String'!!!")
+        if type(get_unit) != bool:
+            raise TypeError("unit must be a 'Boolean'!!!")
+        if type(unit_change_to) != list and type(unit_change_to) != int:
+            raise TypeError("unit_change must be a 'Integer'!!!")
+
+        ## 把sname轉換成大寫，因為Aspen裡面的名稱都大寫
+        sname = sname.upper()
+
+        ## 檢查輸入的物流名稱是否在檔案當中，如無回傳錯誤
+        if sname not in self.master.StreamsList():
+            raise UDE.AspenPlus_StreamTypeError("Cannot Find " + sname
+                                                + " in the AspenFile. "
+                                                + "Please Check the name you type!!")
+
+        path = "\Data\Streams" + "\\" + sname + "\Output\STR_MAIN\VOLFLMX\MIXED"
+
+        ## 看看有沒有要改單位
+        if unit_change_to == []:  # 沒有要改單位
+            ## 是要取得數值還是單位
+            if get_unit:  # 取得單位
+                return self.master.UnitFind(self.aspen.Tree.FindNode(path), table=True)
+            else:  # 取得數值
+                return self.aspen.Tree.FindNode(path).Value
+        elif unit_change_to != []:  # 要改單位的話
+            if get_unit:  # 取得單位
+                pq = self.aspen.Tree.FindNode(path).AttributeValue(2)
+                return self.master.Unit(item=[pq, unit_change_to], table=True)
+            else:  # 取得數值
+                return self.master.UnitChange(self.aspen.Tree.FindNode(path), unit_change_to)
 
 if __name__ == "__main__":
     import AspenPlus
--- main.py
+++ main.py
@@ -4,97 +4,310 @@ path = "D:\\Google Cloud\\Python\\Great program for AspenPlus\\TestFile\\Distill
 Aspen = AspenPlus.AP(path)
 obj = Aspen.aspen.Tree.FindNode("\Data\Streams\B1\Output\TOT_FLOW")
 
-# 測試Error()
-print(Aspen.Error())
-# 測試Show()
-Aspen.Show(True)
-Aspen.Show(False)
-try:
-    Aspen.Show(123)
-except TypeError:
-    n = 1
-    print('Error' + str(n)) # 應為1
-# 測試Unit()
-Aspen.Unit([])
-Aspen.Unit([],table=True)
-Aspen.Unit([1])
-Aspen.Unit([1],table=True)
-Aspen.Unit([1,1])
-Aspen.Unit([1,1],table=True)
-try:
-    Aspen.Unit(123)
-except TypeError:
-    n += 1
-    print('Error' + str(n)) # 應為2
-try:
-    Aspen.Unit([],123)
-except TypeError:
-    n += 1
-    print('Error' + str(n)) # 應為3
-# 測試UnitFind()
-Aspen.UnitFind(obj)
-Aspen.UnitFind(obj, table=True)
-try:
-    Aspen.UnitFind(123)
-except AttributeError:
-    n += 1
-    print('Error' + str(n)) # 應為4
-try:
-    Aspen.UnitFind(obj,table=123)
-except TypeError:
-    n += 1
-    print('Error' + str(n)) # 應為5
-# 測試UnitChange()
-Aspen.UnitChange(obj, 1)
-try:
-    Aspen.UnitChange(123,1)
-except AttributeError:
-    n += 1
-    print('Error' + str(n))  # 應為6
-try:
-    Aspen.UnitChange(obj, '123')
-except TypeError:
-    n += 1
-    print('Error' + str(n))  # 應為7
-# 測試三種ListXXX()
-Aspen.ListBlocks()
-Aspen.ListStreams()
-Aspen.ListComponents()
-# 測試三種XXXList()
-print(Aspen.BlocksList())
-print(Aspen.StreamsList())
-print(Aspen.ComponentsList())
-# 測試Connections()
-Aspen.Connections('c1')
-print(Aspen.Connections('C1',table=True))
-try:
-    Aspen.Connections('c2')
-except AspenPlus.UserDifineException.AspenPlus_BlockTypeError:
-    n += 1
-    print('Error' + str(n))  # 應為8
-try:
-    Aspen.Connections('c1',table=123)
-except TypeError:
-    n += 1
-    print('Error' + str(n))  # 應為9
-try:
-    Aspen.Connections(123)
-except TypeError:
-    n += 1
-    print('Error' + str(n))  # 應為10
-# 測試BlockType()
-print(Aspen.BlockType('c1'))
-try:
-    Aspen.BlockType('c2')
-except AspenPlus.UserDifineException.AspenPlus_BlockTypeError:
-    n += 1
-    print('Error' + str(n))  # 應為11
-try:
-    Aspen.BlockType(123)
-except TypeError:
-    n += 1
-    print('Error' + str(n))  # 應為12
+def basic_test():
+    n = 0
+    # 測試Error()
+    print(Aspen.Error())
+    # 測試Show()
+    Aspen.Show(True)
+    Aspen.Show(False)
+    try:
+        Aspen.Show(123)
+    except TypeError:
+        n = 1
+        print('Error' + str(n)) # 應為1
+    # 測試Unit()
+    Aspen.Unit([])
+    Aspen.Unit([],table=True)
+    Aspen.Unit([1])
+    Aspen.Unit([1],table=True)
+    Aspen.Unit([1,1])
+    Aspen.Unit([1,1],table=True)
+    try:
+        Aspen.Unit(123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n)) # 應為2
+    try:
+        Aspen.Unit([],123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n)) # 應為3
+    # 測試UnitFind()
+    Aspen.UnitFind(obj)
+    Aspen.UnitFind(obj, table=True)
+    try:
+        Aspen.UnitFind(123)
+    except AttributeError:
+        n += 1
+        print('Error' + str(n)) # 應為4
+    try:
+        Aspen.UnitFind(obj,table=123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n)) # 應為5
+    # 測試UnitChange()
+    Aspen.UnitChange(obj, 1)
+    try:
+        Aspen.UnitChange(123,1)
+    except AttributeError:
+        n += 1
+        print('Error' + str(n))  # 應為6
+    try:
+        Aspen.UnitChange(obj, '123')
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為7
+    # 測試三種ListXXX()
+    Aspen.ListBlocks()
+    Aspen.ListStreams()
+    Aspen.ListComponents()
+    # 測試三種XXXList()
+    print(Aspen.BlocksList())
+    print(Aspen.StreamsList())
+    print(Aspen.ComponentsList())
+    # 測試Connections()
+    Aspen.Connections('c1')
+    print(Aspen.Connections('C1',table=True))
+    try:
+        Aspen.Connections('c2')
+    except AspenPlus.UserDifineException.AspenPlus_BlockTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為8
+    try:
+        Aspen.Connections('c1',table=123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為9
+    try:
+        Aspen.Connections(123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為10
+    # 測試BlockType()
+    print(Aspen.BlockType('c1'))
+    try:
+        Aspen.BlockType('c2')
+    except AspenPlus.UserDifineException.AspenPlus_BlockTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為11
+    try:
+        Aspen.BlockType(123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為12
 
+def stream_test():
+    n = 0
+    # 測試MoleFlow()
+    print(Aspen.Stream.MoleFlow('b1'))
+    print(Aspen.Stream.MoleFlow('b1',get_unit=True))
+    print(Aspen.Stream.MoleFlow('b1',get_unit=False,unit_change_to=1))
+    print(Aspen.Stream.MoleFlow('b1', get_unit=True, unit_change_to=1))
+    print(Aspen.Stream.MoleFlow('b1', component='water'))
+    print(Aspen.Stream.MoleFlow('b1', component='water',get_unit=True))
+    print(Aspen.Stream.MoleFlow('b1', component='water',get_unit=False, unit_change_to=1))
+    print(Aspen.Stream.MoleFlow('b1', component='water',get_unit=True, unit_change_to=1))
+    try:
+        Aspen.Stream.MoleFlow('b2')
+    except AspenPlus.UserDifineException.AspenPlus_StreamTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為1
+    try:
+        Aspen.Stream.MoleFlow(123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為2
+    try:
+        Aspen.Stream.MoleFlow('b1',get_unit=123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為3
+    try:
+        Aspen.Stream.MoleFlow('b1',get_unit=False,unit_change_to=True)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為4
+    try:
+        Aspen.Stream.MoleFlow('b1',component=123, get_unit=False, unit_change_to=1)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為5
+
+    # 測試MoleFrac()
+    print(Aspen.Stream.MoleFrac('b1','water'))
+    try:
+        Aspen.Stream.MoleFrac('b2','water')
+    except AspenPlus.UserDifineException.AspenPlus_StreamTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為6
+    try:
+        Aspen.Stream.MoleFrac('b1','waterr')
+    except AspenPlus.UserDifineException.AspenPlus_ComponentTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為7
+    try:
+        Aspen.Stream.MoleFrac(123,'waterr')
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為8
+    try:
+        Aspen.Stream.MoleFrac('b1',123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為9
+
+    # 測試MassFlow()
+    print(Aspen.Stream.MassFlow('b1'))
+    print(Aspen.Stream.MassFlow('b1',get_unit=True))
+    print(Aspen.Stream.MassFlow('b1',get_unit=False,unit_change_to=1))
+    print(Aspen.Stream.MassFlow('b1', get_unit=True, unit_change_to=1))
+    print(Aspen.Stream.MassFlow('b1', component='water'))
+    print(Aspen.Stream.MassFlow('b1', component='water',get_unit=True))
+    print(Aspen.Stream.MassFlow('b1', component='water',get_unit=False, unit_change_to=1))
+    print(Aspen.Stream.MassFlow('b1', component='water',get_unit=True, unit_change_to=1))
+    try:
+        Aspen.Stream.MassFlow('b2')
+    except AspenPlus.UserDifineException.AspenPlus_StreamTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為10
+    try:
+        Aspen.Stream.MassFlow(123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為11
+    try:
+        Aspen.Stream.MassFlow('b1',get_unit=123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為12
+    try:
+        Aspen.Stream.MassFlow('b1',get_unit=False,unit_change_to=True)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為13
+    try:
+        Aspen.Stream.MassFlow('b1',component=123, get_unit=False, unit_change_to=1)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為14
+
+    # 測試MassFrac()
+    print(Aspen.Stream.MassFrac('b1','water'))
+    try:
+        Aspen.Stream.MassFrac('b2','water')
+    except AspenPlus.UserDifineException.AspenPlus_StreamTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為15
+    try:
+        Aspen.Stream.MassFrac('b1','waterr')
+    except AspenPlus.UserDifineException.AspenPlus_ComponentTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為16
+    try:
+        Aspen.Stream.MassFrac(123,'waterr')
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為17
+    try:
+        Aspen.Stream.MassFrac('b1',123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為18
+
+    # 測試Temperature()
+    print(Aspen.Stream.Temperature('b1'))
+    print(Aspen.Stream.Temperature('b1', get_unit=True))
+    print(Aspen.Stream.Temperature('b1', get_unit=False, unit_change_to=1))
+    print(Aspen.Stream.Temperature('b1', get_unit=True, unit_change_to=1))
+    try:
+        Aspen.Stream.Temperature('b2')
+    except AspenPlus.UserDifineException.AspenPlus_StreamTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為19
+    try:
+        Aspen.Stream.Temperature(123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為20
+    try:
+        Aspen.Stream.Temperature('b1',get_unit=123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為21
+    try:
+        Aspen.Stream.Temperature('b1',get_unit=False,unit_change_to='')
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為22
+
+    # 測試Pressure()
+    print(Aspen.Stream.Pressure('b1'))
+    print(Aspen.Stream.Pressure('b1', get_unit=True))
+    print(Aspen.Stream.Pressure('b1', get_unit=False, unit_change_to=1))
+    print(Aspen.Stream.Pressure('b1', get_unit=True, unit_change_to=1))
+    try:
+        Aspen.Stream.Pressure('b2')
+    except AspenPlus.UserDifineException.AspenPlus_StreamTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為23
+    try:
+        Aspen.Stream.Pressure(123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為24
+    try:
+        Aspen.Stream.Pressure('b1', get_unit=123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為25
+    try:
+        Aspen.Stream.Pressure('b1', get_unit=False, unit_change_to='')
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為26
+
+    # 測試VaporFrac()
+    print(Aspen.Stream.VaporFrac('b1'))
+    try:
+        Aspen.Stream.VaporFrac('b2')
+    except AspenPlus.UserDifineException.AspenPlus_StreamTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為27
+    try:
+        Aspen.Stream.VaporFrac(123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為28
+
+    # 測試VolumeFlow()
+    print(Aspen.Stream.VolumeFlow('b1'))
+    print(Aspen.Stream.VolumeFlow('b1', get_unit=True))
+    print(Aspen.Stream.VolumeFlow('b1', get_unit=False, unit_change_to=1))
+    print(Aspen.Stream.VolumeFlow('b1', get_unit=True, unit_change_to=1))
+    try:
+        Aspen.Stream.VolumeFlow('b2')
+    except AspenPlus.UserDifineException.AspenPlus_StreamTypeError:
+        n += 1
+        print('Error' + str(n))  # 應為29
+    try:
+        Aspen.Stream.VolumeFlow(123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為30
+    try:
+        Aspen.Stream.VolumeFlow('b1', get_unit=123)
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為31
+    try:
+        Aspen.Stream.VolumeFlow('b1', get_unit=False, unit_change_to='')
+    except TypeError:
+        n += 1
+        print('Error' + str(n))  # 應為32
+
+# basic_test()
+stream_test()
 
 ## 測試用的兩個參數路徑
 ## Application.Tree.Data.Streams.B1.Output.TOT_FLOW
